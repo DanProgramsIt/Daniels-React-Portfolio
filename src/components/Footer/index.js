c386790875c1791be4608a812139f8e1f4625cd6
Daniel Mendez <soliddan211@gmail.com>
2022-04-17 19:29:19 -0400

    added code to the Footer file

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section>
+      <div className="footer">
+        <p>Daniel Mendez ©</p>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
