329351d82b13d3ef1f0b3626435ac7534a2c330f
Daniel Mendez <soliddan211@gmail.com>
2022-04-17 20:10:51 -0400

    added code to Navigation

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import Navbar from "react-bootstrap/Navbar";
+
+function Navigation(props) {
+  const tabs = ["About", "Portfolio", "Resume", "Contact"];
+
+  return (
+    <header className="navegation">
+      <h2>
+        <FontAwesomeIcon icon={faCode} style={{ fontSize: "20px" }} />
+        Daniel Mendez
+      </h2>
+      <>
+        <div>
+          <Navbar>
+            <ul className="nav nav-tabs justify-content-end">
+              {tabs.map((tab) => (
+                <li className="nav-item" key={tab}>
+                  <a
+                    href={"#" + tab.toLowerCase()}
+                    onClick={() => props.setCurrentPage(tab)}
+                    className={
+                      props.currentPage === tab ? "navActive" : "nav-link"
+                    }
+                  >
+                    {tab}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </Navbar>
+        </div>
+      </>
+    </header>
+  );
+}
+
+export default Navigation;
